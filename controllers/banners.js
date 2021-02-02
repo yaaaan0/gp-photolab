1701ea44735c708531b78510d6a269bcec2728f4
yaaaan0 <70134635+yaaaan0@users.noreply.github.com>
2021-02-02 09:59:24 +0800

    Update banners.js

--- controllers/banners.js
+++ controllers/banners.js
@@ -183,8 +183,8 @@ export const file = async (req, res) => {
       method: 'GET',
       url: 'http://' + process.env.FTP_HOST + '/' + process.env.FTP_USER + '/' + req.params.file,
       responseType: 'stream'
-    }).then(res => {
-      res.data.pipe(res)
+    }).then(ress => {
+      ress.data.pipe(res)
     }).catch(error => {
       res.status(error.response.status).send({ success: false, message: '取得圖片失敗' })
       console.log(error)
